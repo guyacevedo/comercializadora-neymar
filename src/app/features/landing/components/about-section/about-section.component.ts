929f834980405ab7205b40a4ca25704712d2996f
guyacevedo <guy.acevedoa@gmail.com>
2025-10-13 16:02:09 -0500

    I created section -about

---
+++ src/app/features/landing/components/about-section/about-section.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-section',
+  imports: [],
+  templateUrl: './about-section.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AboutSectionComponent {
+
+}
--- src/app/features/landing/pages/home-page/home-page.component.ts
+++ src/app/features/landing/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeroSectionComponent } from "../../components/hero-section/hero-section.component";
+import { AboutSectionComponent } from "../../components/about-section/about-section.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent, AboutSectionComponent],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
