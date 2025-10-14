135630ffbcc0be571fe215a85b4f742a68c2bc73
guyacevedo <guy.acevedoa@gmail.com>
2025-10-13 21:48:11 -0500

    I created testimonials-section

--- src/app/features/landing/pages/home-page/home-page.component.ts
+++ src/app/features/landing/pages/home-page/home-page.component.ts
@@ -5,10 +5,11 @@ import { ProductsSectionComponent } from "../../components/products-section/prod
 import { ServicesSectionComponent } from "../../components/services-section/services-section.component";
 import { AdvantagesSectionComponent } from "../../components/advantages-section/advantages-section.component";
 import { NewsSectionComponent } from "../../components/news-section/news-section.component";
+import { TestimonialsSectionComponent } from "../../components/testimonials-section/testimonials-section.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSectionComponent, AboutSectionComponent, ProductsSectionComponent, ServicesSectionComponent, AdvantagesSectionComponent, NewsSectionComponent],
+  imports: [HeroSectionComponent, AboutSectionComponent, ProductsSectionComponent, ServicesSectionComponent, AdvantagesSectionComponent, NewsSectionComponent, TestimonialsSectionComponent],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
